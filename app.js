51a2d74213ae393cd494048eec03a468d49dbcca
ArzumResulova <seidresul2021@gmail.com>
2025-07-03 01:03:10 +0400

    a new page has been formed

--- app.js
+++ app.js
@@ -1,6 +1,89 @@
 /* DO NOT DESTROY */
 
 
+const formSignUp = document.querySelector("#formSignUp");
+
+const NameInp = document.querySelector(".NameInp");
+
+const PassInp = document.querySelector(".PassInp");
+
+const EmailInp = document.querySelector(".EmailInp");
+
+let gif = document.querySelector(".gif");
+
+let btnForm = document.querySelector(".btnForm");
+
+let alertoFName = document.querySelector(".alertoFName")
+
+
+
+
+
+
+
+
+const objectInp = {
+    name: "",
+    pass: "",
+    email: ""
+}
+
+
+NameInp?.addEventListener("change", (e) => {
+
+    objectInp.name = e.target.value;
+
+})
+PassInp?.addEventListener("change", (e) => {
+
+    objectInp.pass = e.target.value;
+
+})
+EmailInp?.addEventListener("change", (e) => {
+
+    objectInp.email = e.target.value;
+
+})
+
+
+
+console.log(localStorage);
+
+
+
+
+
+formSignUp?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(objectInp);
+
+
+    localStorage.setItem("ObjectInp", JSON.stringify(objectInp));
+
+    if (NameInp.value === "" || PassInp.value === "" || EmailInp.value === "") {
+        NameInp.style.border = "1px,solid,red"
+        PassInp.style.border = "1px,solid,red"
+        EmailInp.style.border = "1px,solid,red"
+
+
+    }else {
+        location.href="login.html"
+    }
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
 const menu = document.querySelector(".menu");
 
 
@@ -9,7 +92,9 @@ const burgerMenu = document.querySelector(".burger-menu");
 
 
 
-burgerMenu.addEventListener("click", () => {
+burgerMenu?.addEventListener("click", () => {
+
+    console.log("salam");
 
 
     console.log(menu.getAttribute("style"));
@@ -25,13 +110,89 @@ burgerMenu.addEventListener("click", () => {
     }
 })
 
+// LogIn starts
+
+
+
+
+
+const formLogin = document.querySelector("#formLogin");
+const NameLogin = document.querySelector(".NameLogin");
+const PassLogin = document.querySelector(".PassLogin");
+
+
+let local = JSON.parse(localStorage.getItem("ObjectInp"));
+
+
+
+
+const AllInp = {
+    name: "",
+    password: ""
+
+}
+
+
+NameLogin?.addEventListener("change", (e) => {
+
+    AllInp.name = e.target.value.trim();
+})
+
+PassLogin?.addEventListener("change", (e) => {
+
+    AllInp.password = e.target.value.trim();
+})
+
+
+
+
+formLogin?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    console.log(local.name, AllInp.name, local.pass, AllInp.password);
+
+
+    if (AllInp.name == "" && AllInp.password == "") {
+        alert("fill them");
+
+    }
+
+    else if (local.name === AllInp.name && local.pass === AllInp.password) {
+
+
+        btnForm.style.gap = "20px";
+
+        gif.style.display = "flex";
+        setTimeout(() => {
+
+            location.href = "../pages/home.html"
+        }, 2000)
+
+    } else if (AllInp.name == "") {
+        NameLogin.style.backgroundColor = "red"
+        alertoFName.style.opacity="1";
+
+    } else if (AllInp.password == "") {
+        PassLogin.style.backgroundColor = "red"
+        alertoFName.style.opacity = "1";
+    }
+
+    else {
+
+        alert("Login veyaxud parol shedir")
+
+    }
+})
 
 
 
 
 
 
-
+// btnForm.style.gap = "20px";
+// gif.style.display = "flex";
+// NameLogin.style.border = "1px solid red"
+// PassLogin.style.border = "1px solid red"
 
 
 
